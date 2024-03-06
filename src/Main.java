e777feeb07502aa7091f1165c1d91f568f4a1664
berko <ethan.c.berkowitz@gmail.com>
2024-03-06 12:36:49 -0500

    tictactoe lol

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static ArrayList<Integer> master = new ArrayList<Integer>();
+    static int idx;
 
     public static void main(String[] args) throws IOException {
         File in = new File("in.txt");
         BufferedReader br = new BufferedReader(new FileReader(in));
-        char c = (char)br.read();
-        int idx = 0;
+        idx = 0;
         master.add(0);
+        parse(br);
+        br.close();
+    }
+
+    public static void parse(BufferedReader br) throws IOException {
+        char c = (char)br.read();
         while(c != ':') {
             switch(c) {
                 case '>':
@@ -19,7 +26,8 @@ public class Main {
                     int inc = 0;
                     if(c == '[') {
                         while((c = (char)br.read())!=']') {
-                            inc = inc * 10 + (int)(c-'0');
+                            if(c == '.') inc = inc * 10 + master.get(idx);
+                            else inc = inc * 10 + (int)(c-'0');
                         }
                         c = (char)br.read();
                     } else {
@@ -37,7 +45,8 @@ public class Main {
                     inc = 0;
                     if(c == '[') {
                         while((c = (char)br.read())!=']') {
-                            inc = inc * 10 + (int)(c-'0');
+                            if(c == '.') inc = inc * 10 + master.get(idx);
+                            else inc = inc * 10 + (int)(c-'0');
                         }
                         c = (char)br.read();
                     } else {
@@ -46,12 +55,17 @@ public class Main {
                     idx -= inc;
                     if(idx<0) idx = 0;
                     break;
+                case '_':
+                    idx = 0;
+                    c = (char)br.read();
+                    break;
                 case '+':
                     c = (char)br.read();
                     inc = 0;
                     if(c == '[') {
                         while((c = (char)br.read())!=']') {
-                            inc = inc * 10 + (int)(c-'0');
+                            if(c == '.') inc = inc * 10 + master.get(idx);
+                            else inc = inc * 10 + (int)(c-'0');
                         }
                         c = (char)br.read();
                     } else {
@@ -64,7 +78,8 @@ public class Main {
                     inc = 0;
                     if(c == '[') {
                         while((c = (char)br.read())!=']') {
-                            inc = inc * 10 + (int)(c-'0');
+                            if(c == '.') inc = inc * 10 + master.get(idx);
+                            else inc = inc * 10 + (int)(c-'0');
                         }
                         c = (char)br.read();
                     } else {
@@ -87,10 +102,39 @@ public class Main {
                     }
                     c = (char)br.read();
                     break;
-                case default:
+                case '@':
+                    StringBuilder chars = new StringBuilder();
+                    while((c = (char)br.read()) != '}') {
+                        chars.append(c);
+                    }
+                    File temp = new File("temp\\" + master.get(idx) + ".txt");
+                    temp.deleteOnExit();
+                    BufferedWriter bw = new BufferedWriter(new FileWriter(temp));
+                    bw.write(chars.toString());
+                    bw.flush();
+                    bw.close();
+                    c = (char)br.read();
+                    break;
+                case '#':
+                    BufferedReader b = new BufferedReader(new FileReader("temp\\" + master.get(idx) + ".txt"));
+                    parse(b);
+                    b.close();
+                    c = (char)br.read();
+                    break;
+                case '$':
+                    System.out.println("DEBUG: value " + master.get(idx) +  " at idx " + idx );
+                    c = (char)br.read();
+                    break;
+                case '?':
+                    System.out.println("DEBUG: master array: {" + master.stream().map(Object::toString)
+                            .collect(Collectors.joining(", ")) + "}");
+                    c = (char)br.read();
+                    break;
+                default:
                     c = (char)br.read();
                     break;
             }
         }
     }
+
 }
